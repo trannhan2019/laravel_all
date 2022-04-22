7b7215ed540d3d2e9509862e6b3a0486bcb23a6b
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2022-04-22 17:45:34 +0700

    22.04-cf

--- config/fortify.php
+++ config/fortify.php
@@ -61,7 +61,8 @@ return [
     |
     */
 
-    'home' => RouteServiceProvider::HOME,
+    // 'home' => RouteServiceProvider::HOME,
+    'home' => env('SPA_URL').'/dashboard',
 
     /*
     |--------------------------------------------------------------------------
