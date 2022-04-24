9f8d01a92d18a0f44abd171001d85b08e90be57f
TranNhan <trannhan.s3@gmail.com>
2022-04-24 15:20:03 +0700

    24.04.2022

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    //...
+      Route::get('/users/{id}', function ($id) {
+        return User::findOrFail($id);
+    });
+  });
